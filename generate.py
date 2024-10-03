c99f4ace8f741032a7cce5b160f6f5ae54616a19
csprinkl <csprinkl@uab.edu>
2024-10-02 18:12:08 -0700

    Slightly improved response to prompts by adding more parameters

--- generate.py
+++ generate.py
@@ -2,8 +2,18 @@ import model
 
 def generate_response(prompt): #Take user input prompt and generate a GPT 2 bot response using the parameters defined.
     inputs = model.tokenizer.encode(prompt, return_tensors="pt")
-    outputs = model.model.generate(inputs, max_length=150, do_sample=True, top_p=0.95, top_k=60)
-    response = model.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    attention_mask = (inputs != model.tokenizer.pad_token_id).long()
+    outputs = model.model.generate(
+        inputs, 
+        attention_mask=attention_mask, 
+        max_length=100, 
+        do_sample=True, 
+        top_p=0.9, 
+        top_k=30, 
+        temperature=0.3,
+        no_repeat_ngram_size=3 #Prevent repetition of trigrams
+        )
+    response = model.tokenizer.decode(outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
     return response
 
 conversation_history = [] #List to hold conversation history of the current bot interaction.
--- model.py
+++ model.py
@@ -1,5 +1,10 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
 
 model_name = "gpt2"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+#Set pad_token_id
+tokenizer.pad_token = tokenizer.eos_token #Set pad token to be equal to the eos token
+model.config.pad_token_id = tokenizer.pad_token_id
